4683b8a4807d777aa39ecef9c4065967e624d414
Ilya Andriyanov <80912805+Regikon@users.noreply.github.com>
2025-10-03 10:37:01 +0300

    feat (FSO-67): добавлен solid.js (#2)

---
+++ src/components/app/app.tsx
@@ -0,0 +1,14 @@
+import './app.css'
+
+function App() {
+  return (
+    <>
+      <h1>final-project-vk-frontend</h1>
+      <div>
+        <p>Hello, World!</p>
+      </div>
+    </>
+  )
+}
+
+export default App
--- src/main.ts
+++
@@ -1,8 +0,0 @@
-import './global_style.css';
-
-const app = document.querySelector<HTMLDivElement>('#app')!;
-app.innerHTML = `
-  <div>
-    Hello, World!
-  </div>
-`;
