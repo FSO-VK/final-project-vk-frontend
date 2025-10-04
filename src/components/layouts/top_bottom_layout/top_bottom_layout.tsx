d1569d100565a4a70dcc0d3a15f7cecf1ba6551e
Ilya Andriyanov <ilyaandry35@gmail.com>
2025-10-04 15:06:54 +0300

    fix (NO-TASK): установлены пределы для ширины вертикального layout

--- src/components/layouts/top_bottom_layout/top_bottom_layout.tsx
+++ src/components/layouts/top_bottom_layout/top_bottom_layout.tsx
@@ -1,5 +1,6 @@
 import type { JSXElement } from 'solid-js';
 import Header from '../header/header.tsx';
+import "./top_bottom_layout.css"
 
 export interface TopBottomLayoutProps {
   children?: JSXElement[] | JSXElement;
@@ -9,7 +10,9 @@ function TopBottomLayout(props: TopBottomLayoutProps) {
   return (
     <>
       <Header />
-      {props.children}
+      <main class='page-container'>
+        {props.children}
+      </main>
     </>
   );
 }
