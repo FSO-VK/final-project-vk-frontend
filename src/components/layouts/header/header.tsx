aacaf2886b05b81694ef1445bdd6f75b0a7656fb
Ilya Andriyanov <ilyaandry35@gmail.com>
2025-10-06 16:04:17 +0300

    ref (NO-TASK) отформатированы все исходники

--- src/components/layouts/header/header.tsx
+++ src/components/layouts/header/header.tsx
@@ -1,9 +1,11 @@
 import './header.css';
 
 function Header() {
-  return <nav class="header">
-    <h1>Моя аптечка</h1>
-  </nav>;
+  return (
+    <nav class="header">
+      <h1>Моя аптечка</h1>
+    </nav>
+  );
 }
 
 export default Header;
