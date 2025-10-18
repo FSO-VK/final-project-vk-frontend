a051fd3611378f4362c628fd51f615ec1ecc880d
Ilya Andriyanov <ilyaandry35@gmail.com>
2025-10-18 22:37:01 +0300

    fix (FSO-132): navbar layout параметризован

--- src/app/ui/layouts/navbar/navbar.tsx
+++ src/app/ui/layouts/navbar/navbar.tsx
@@ -1,13 +1,22 @@
-import type { ParentComponent } from 'solid-js';
 import './navbar.css';
 import { Divider, Navbar } from '@/shared/ui';
+import type { ParentProps } from 'solid-js';
+
+export interface NavbarLayoutProps extends ParentProps {
+  navBarTitle: string;
+  navBarBackLocation: string;
+}
 
 // NavbarLayout is a fullscreen layout with navigation bar on top,
 // bounded by horizontal size on desktops.
-export const NavbarLayout: ParentComponent = (props) => {
+export const NavbarLayout = (props: NavbarLayoutProps) => {
   return (
     <div class="navbar-layout">
-      <Navbar elementClass="navbar-layout__navbar" title="Очень длинное название" backLocation="" />
+      <Navbar
+        elementClass="navbar-layout__navbar"
+        title={props.navBarTitle ?? ''}
+        backLocation={props.navBarBackLocation ?? ''}
+      />
       <Divider />
       <div class="navbar-layout__page">{props.children}</div>
     </div>
