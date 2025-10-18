ea2f82e57739e43f27dd25f391bd47b3fe163647
Ilya Andriyanov <80912805+Regikon@users.noreply.github.com>
2025-10-18 16:38:18 +0300

    feat (FSO-119): добавлен layout для страниц (#17)
    * feat (FSO-119): добавлен layout для страниц
    
    * fix (FSO-119) игнор скринридера
    
    * fix (FSO-119) добавлено расширение ридми

---
+++ src/app/ui/layouts/fullscreen_fixed/fullscreen_fixed.tsx
@@ -0,0 +1,7 @@
+import type { ParentComponent } from 'solid-js';
+import './fullscreen_fixed.css';
+
+// FullscreenFixedLayout is a fullscreen layout, bouded by horizontal size on desktops
+export const FullscreenFixedLayout: ParentComponent = (props) => {
+  return <div class="fullscreen-fixed">{props.children}</div>;
+};
