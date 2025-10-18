8656e18fc724baa68b78fc77eaa9d0136b050772
Ilya Andriyanov <ilyaandry35@gmail.com>
2025-10-18 15:39:49 +0300

    fix (FSO-119) игнор скринридера

--- src/app/ui/layouts/fullscreen_fixed/fullscreen_fixed.tsx
+++ src/app/ui/layouts/fullscreen_fixed/fullscreen_fixed.tsx
@@ -3,9 +3,5 @@ import './fullscreen_fixed.css';
 
 // FullscreenFixedLayout is a fullscreen layout, bouded by horizontal size on desktops
 export const FullscreenFixedLayout: ParentComponent = (props) => {
-  return (
-    <div class="fullscreen-fixed" aria-hidden="true">
-      {props.children}
-    </div>
-  );
+  return <div class="fullscreen-fixed">{props.children}</div>;
 };
