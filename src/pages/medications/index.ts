b3ac3660c7bec776ee118fdab015062340b8db05
Ilya Andriyanov <ilyaandry35@gmail.com>
2025-10-28 14:27:35 +0300

    fix (NO-TASK) убрана страница препаратов

--- src/pages/medications/index.ts
+++
@@ -1,2 +0,0 @@
-export { MedicationsPage } from './ui/medications/medications';
-export { MedicationCard } from './ui/medication_card/medication_card';
--- src/shared/api/index.ts
+++ src/shared/api/index.ts
@@ -11,6 +11,7 @@ export type {
   UpdateMedicationOptions,
   UpdateMedicationDTO,
   DeleteMedicatoinOptions,
+  Medication,
   ScanMedicationDTO,
 } from './endpoints/medication';
 
