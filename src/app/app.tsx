7baca15db8e258f070767b368f7319a14decd1c1
Ilya Andriyanov <ilyaandry35@gmail.com>
2025-11-12 15:39:54 +0300

    fix (FSO-115): препарат кодируется urlencoded

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -105,7 +105,7 @@ export function App(props: AppProps) {
                 return (
                   <MedicationsListPage
                     onScanned={(result) => {
-                      navigate(`/medications/add?data=${result}`);
+                      navigate(`/medications/add?data=${encodeURIComponent(result)}`);
                     }}
                   />
                 );
