60adeb9b4b8645947e2a3ae25ee70c6ba70b2611
Ilya Andriyanov <ilyaandry35@gmail.com>
2025-11-20 09:04:40 +0300

    fix (FSO-205) починена логика switch

--- src/pages/profile_page/ui/profile-notification-section.tsx
+++ src/pages/profile_page/ui/profile-notification-section.tsx
@@ -5,7 +5,7 @@ import {
   unsubscribeUserFromPush,
 } from '@/features/notification';
 import { SwitchButton } from '@/shared/ui';
-import { Suspense, createEffect } from 'solid-js';
+import { createEffect } from 'solid-js';
 import { createAsync } from '@solidjs/router';
 import { createForm } from '@tanstack/solid-form';
 import { assertIfError } from '@/shared/lib';
@@ -13,16 +13,20 @@ import { assertIfError } from '@/shared/lib';
 export function ProfileNotificationSection() {
   const isSubscribed = createAsync(() => isUserSubscribed());
 
-  const handleSubmit = async (isSubscribed: boolean) => {
-    if (!isSubscribed) {
+  const handleSubmit = async (wantToSubscribe: boolean) => {
+    if (wantToSubscribe) {
       const hasPermission = await askNotificationPermission();
       if (hasPermission) {
         await subscribeUserToPush();
+      } else {
+        throw new Error('user has not given access to notifications');
       }
     } else {
       await unsubscribeUserFromPush();
     }
   };
+
+  const REVERT_SWITCH_STATE_TIMEOUT = 400; // ms
 
   const form = createForm(() => ({
     defaultValues: {
@@ -34,17 +38,15 @@ export function ProfileNotificationSection() {
       } catch (e: unknown) {
         assertIfError(e);
         console.error('failed to change subscription state, reverting it:', e);
-        form.setFieldValue('subscription', !value.subscription);
+        setTimeout(() => {
+          form.setFieldValue('subscription', !value.subscription);
+        }, REVERT_SWITCH_STATE_TIMEOUT);
       }
     },
   }));
 
   createEffect(() => {
-    const subscribeValue = isSubscribed();
-
-    if (subscribeValue !== undefined) {
-      form.setFieldValue('subscription', subscribeValue);
-    }
+    form.setFieldValue('subscription', isSubscribed() ?? false);
   });
 
   return (
@@ -53,25 +55,23 @@ export function ProfileNotificationSection() {
       <div class="profile-page__notification-subscribe-container">
         Подписаться на уведомления
         <form>
-          <Suspense>
-            <form.Field
-              name="subscription"
-              children={(field) => (
-                <SwitchButton
-                  name={field().name}
-                  id={field().name}
-                  checked={field().state.value}
-                  onChange={(e) => {
-                    const isChecked = e.target.checked;
-                    field().handleChange(isChecked);
-                    form.handleSubmit().catch(() => {
-                      console.error("can't change subscription state");
-                    });
-                  }}
-                />
-              )}
-            />
-          </Suspense>
+          <form.Field
+            name="subscription"
+            children={(field) => (
+              <SwitchButton
+                name={field().name}
+                id={field().name}
+                checked={field().state.value}
+                onChange={(e) => {
+                  const isChecked = e.target.checked;
+                  field().handleChange(isChecked);
+                  form.handleSubmit().catch(() => {
+                    console.error("can't change subscription state");
+                  });
+                }}
+              />
+            )}
+          />
         </form>
       </div>
     </section>
