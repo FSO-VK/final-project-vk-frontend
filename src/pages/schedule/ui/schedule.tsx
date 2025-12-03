745894123fa4d229e60f33b8aae68e175f843761
Ifelsik <misha2004546@gmail.com>
2025-12-03 13:23:56 +0300

    feat (FSO-268): компонент календаря

--- src/pages/schedule/ui/schedule.tsx
+++ src/pages/schedule/ui/schedule.tsx
@@ -1,10 +1,32 @@
-import { CalendarRowSection } from './calendar';
+import { createSignal } from 'solid-js';
+import { CalendarRowSection, Day } from './calendar';
 
 export function SchedulePage() {
+  const days: Day[] = [
+    { dayOfWeek: 'пн', day: 1 },
+    { dayOfWeek: 'вт', day: 2 },
+    { dayOfWeek: 'ср', day: 3 },
+    { dayOfWeek: 'чт', day: 4 },
+    { dayOfWeek: 'пт', day: 5 },
+    { dayOfWeek: 'сб', day: 6 },
+    { dayOfWeek: 'вс', day: 7 },
+  ];
+
+  const currentDay = 3;
+
+  const [selectedDay, setSelectedDay] = createSignal(currentDay);
+
   return (
     <main>
       <section>
-        <CalendarRowSection />
+        <CalendarRowSection
+          days={days}
+          today={currentDay}
+          currentDay={selectedDay()}
+          onDateClick={(day: number) => {
+            setSelectedDay(day);
+          }}
+        />
       </section>
     </main>
   );
