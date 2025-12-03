97ff3094660f47aa76f0f6984e51c52971d70926
Ifelsik <misha2004546@gmail.com>
2025-12-03 16:11:28 +0300

    feat (FSO-272): страничка расписания

---
+++ src/pages/schedule/ui/date.ts
@@ -0,0 +1,34 @@
+import { type Day } from './calendar';
+
+export function getDateFormatted(date: Date): string {
+  const formatter = new Intl.DateTimeFormat('ru-RU', {
+    weekday: 'long',
+    day: 'numeric',
+    month: 'long',
+  });
+  const formatted = formatter.format(date);
+  return formatted.charAt(0).toUpperCase() + formatted.slice(1);
+}
+
+export function getCurrentWeek(time: Date): Day[] {
+  const dayOfWeek = time.getDay();
+
+  // because week starts from sunday
+  const durationFromMonday = dayOfWeek === 0 ? 6 : dayOfWeek - 1;
+
+  const day = new Date(time.getDate() - durationFromMonday);
+
+  const formatter = new Intl.DateTimeFormat('ru-RU', {
+    weekday: 'short',
+  });
+  const week: Day[] = [];
+  for (let i = 0; i < 7; i++) {
+    const current = new Date(day);
+    week.push({
+      day: current.getDate(),
+      dayOfWeek: formatter.format(current),
+    });
+    day.setDate(day.getDate() + 1);
+  }
+  return week;
+}
