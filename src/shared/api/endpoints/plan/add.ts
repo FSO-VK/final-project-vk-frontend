4f65d661afe3fe02574fb9194aec0f9be5cc8d1f
Ilya Andriyanov <ilyaandry35@gmail.com>
2026-01-12 14:12:45 +0300

    feat (FSO-309): обновлено API добавления плана

--- src/shared/api/endpoints/plan/add.ts
+++ src/shared/api/endpoints/plan/add.ts
@@ -30,35 +30,11 @@ export const AddPlanDTO = z.object({
   recurrenceRule: z.array(z.string()),
 });
 
-function toIsoString(date: Date) {
-  const tzo = -date.getTimezoneOffset(),
-    dif = tzo >= 0 ? '+' : '-',
-    pad = function (num: number) {
-      return (num < 10 ? '0' : '') + num;
-    };
-
-  return (
-    date.getFullYear() +
-    '-' +
-    pad(date.getMonth() + 1) +
-    '-' +
-    pad(date.getDate()) +
-    'T' +
-    pad(date.getHours()) +
-    ':' +
-    pad(date.getMinutes()) +
-    ':' +
-    pad(date.getSeconds()) +
-    dif +
-    pad(Math.floor(Math.abs(tzo) / 60)) +
-    ':' +
-    pad(Math.abs(tzo) % 60)
-  );
-}
-
 export async function add(options: AddPlanOptions): Promise<z.infer<typeof AddPlanDTO>> {
   const body = await backendClient.post('/planning/plan', {
-    body: { ...options, startDate: toIsoString(options.startDate) },
+    body: {
+      ...options,
+    },
     useCredentials: true,
   });
   return AddPlanDTO.parse(body);
