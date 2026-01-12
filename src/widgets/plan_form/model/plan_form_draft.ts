083ae3713c3c418a1870343058a0f48c16e930fe
Ilya Andriyanov <ilyaandry35@gmail.com>
2026-01-12 14:12:45 +0300

    feat (FSO-309): обновлено API добавления плана

--- src/widgets/plan_form/model/plan_form_draft.ts
+++ src/widgets/plan_form/model/plan_form_draft.ts
@@ -1,6 +1,10 @@
 import { Medication } from '@/entities/medication';
 import { PlanDayPeriod, PlanDraft } from '@/entities/plan';
 
+// Some common js module import magic.
+import * as rrule from 'rrule';
+const { RRule, datetime } = rrule;
+
 export interface PlanFormDraft {
   medication: Medication;
   period: PlanDayPeriod;
@@ -11,21 +15,42 @@ export interface PlanFormDraft {
   timeOfIntake: string[];
 }
 
+const jsDateToDatetime = (jsD: Date) => {
+  return datetime(
+    jsD.getUTCFullYear(),
+    jsD.getUTCMonth(),
+    jsD.getUTCDay(),
+    jsD.getUTCHours(),
+    jsD.getUTCMinutes(),
+    jsD.getUTCSeconds(),
+  );
+};
+
 export function adaptPlanFormDraft(pfd: PlanFormDraft): PlanDraft {
-  let recurrenceRulePrefix: string;
-
-  switch (pfd.period) {
-    case PlanDayPeriod.EveryDay:
-      recurrenceRulePrefix = 'FREQ=DAILY;';
-      break;
-    case PlanDayPeriod.EveryXDays:
-      recurrenceRulePrefix = `FREQ=DAILY;INTERVAL=${pfd.intakeDaysInterval};`;
-      break;
-  }
-
   const recurrenceRule = pfd.timeOfIntake.map((time) => {
     const [hour, min] = time.split(':');
-    return recurrenceRulePrefix + `BYHOUR=${hour};BYMINUTE=${min}`;
+    const actualStart = new Date(pfd.intakeStart);
+    actualStart.setHours(Number(hour), Number(min));
+
+    let singleRule: rrule.RRule;
+
+    switch (pfd.period) {
+      case PlanDayPeriod.EveryDay:
+        singleRule = new RRule({
+          freq: RRule.DAILY,
+          dtstart: jsDateToDatetime(actualStart),
+        });
+        break;
+      case PlanDayPeriod.EveryXDays:
+        singleRule = new RRule({
+          freq: RRule.DAILY,
+          interval: pfd.intakeDaysInterval,
+          dtstart: jsDateToDatetime(actualStart),
+        });
+        break;
+    }
+
+    return singleRule.toString();
   });
 
   return {
